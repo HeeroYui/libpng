99e6413c795826d718199b174eec6e79bb7fc8de
Edouard DUPIN <yui.heero@gmail.com>
2012-08-10 13:23:01 +0200

    simplify make

--- Android.mk
+++ Android.mk
@@ -16,11 +16,9 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE    := libpng
-
 LOCAL_CFLAGS := -DPNG_NO_LIMITS_H
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)
+LOCAL_MODULE    := libpng
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
 
